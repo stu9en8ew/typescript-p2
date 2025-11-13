14d590bb47a451631efb7c20dbfe6d2c3c4501c4
Márcio Marques <mar.info.rj@gmail.com>
2025-11-13 23:03:46 +0100

    creazione di negociacoes-view

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,17 @@
+export class NegociacoesView {
+
+    template(): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+            <tr>
+                <th>DATA
+                <th>QUANTIDADE
+                <th>VALOR
+            </tr>
+            <tbody>
+            </tbody>
+        </table>
+        `;
+    }
+}
